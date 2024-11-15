5766d374436f5c359e678dd2114bdfec61aebabf
8AwA8 <113742992+8AwA8@users.noreply.github.com>
2024-11-15 16:47:42 -0330

    Add files via upload

---
+++ MainProjUnzip/BossBattlesEntry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using StardewModdingAPI;
+
+namespace BossBattles
+{
+	// Token: 0x02000004 RID: 4
+	public class BossBattlesEntry : Mod
+	{
+		// Token: 0x06000003 RID: 3 RVA: 0x00002050 File Offset: 0x00000250
+		public override void Entry(IModHelper helper)
+		{
+		}
+	}
+}
